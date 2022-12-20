1f3b6be50b63f35942dbcaebd82207bd7ee6c90b
mikekondo <foad5969@gmail.com>
2022-12-20 16:05:07 +0900

    add: ライブラリviewの作成

--- SwiftUI-SpotifyApp/Views/HomeView.swift
+++ SwiftUI-SpotifyApp/Views/HomeView.swift
@@ -11,6 +11,24 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
+            HStack() {
+                Image("favorite_music")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40,height: 40)
+                Text("お気に入りの曲")
+                    .foregroundColor(.white)
+                    .bold()
+                    .font(.system(size: 10, weight: .black, design: .default))
+                    .padding(EdgeInsets(
+                      top: 0,        // 上の余白
+                      leading: 0,    // 左の余白
+                      bottom: 0,     // 下の余白
+                      trailing: 10    // 右の余白
+                      ))
+            }
+            .background(Color(red: 35/255, green: 5/255, blue: 5/255))
+                .cornerRadius(3)
         }
     }
 }
