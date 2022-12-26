9f42f7edf4ef9585a4352b0e7314e1dad36ca9d4
mikekondo <foad5969@gmail.com>
2022-12-26 16:52:20 +0900

    add: HomeLibraryViewのレイアウト調整

--- SwiftUI-SpotifyApp/Views/Home/HomeLibraryView.swift
+++ SwiftUI-SpotifyApp/Views/Home/HomeLibraryView.swift
@@ -8,35 +8,23 @@
 import SwiftUI
 
 struct HomeLibraryView: View {
+    // MARK: - Variables
     @State var homeLibraryArray = [HomeLibrary(name: "お気に入りの曲", image: "favorite_music"),HomeLibrary(name: "the chain smokers", image: "chain"),HomeLibrary(name: "Discover Weekly", image: "discover_weekly"),HomeLibrary(name: "Best of Tokyo", image: "Best_of_tokyo"),HomeLibrary(name: "Next Up", image: "Next_up"),HomeLibrary(name: "バラード", image: "ballad")]
+
+    var columns: [GridItem] = Array(repeating: .init(.flexible(),spacing: 0,alignment: .center), count: 2)
+
+    @State private var selection = 0
+
+    // MARK: - View
     var body: some View {
-//        HStack() {
-//            Image(homeLibrary.image)
-//                .resizable()
-//                .aspectRatio(contentMode: .fit)
-//                .frame(width: 40,height: 40)
-//            Text(homeLibrary.name)
-//                .foregroundColor(.white)
-//                .bold()
-//                .font(.system(size: 10, weight: .black, design: .default))
-//                .padding(EdgeInsets(
-//                  top: 0,        // 上の余白
-//                  leading: 0,    // 左の余白
-//                  bottom: 0,     // 下の余白
-//                  trailing: 10    // 右の余白
-//                  ))
-//        }
-//        .background(Color(red: 35/255, green: 5/255, blue: 5/255))
-//            .cornerRadius(3)
-        VStack {
-            ScrollView(.vertical) {
-                ForEach(homeLibraryArray) { homeLibrary in
+            VStack {
+                HStack() {
                     HStack() {
-                        Image(homeLibrary.image)
+                        Image(homeLibraryArray[0].image)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 40,height: 40)
-                        Text(homeLibrary.name)
+                            .frame(width: 50,height: 50)
+                        Text(homeLibraryArray[0].name)
                             .foregroundColor(.white)
                             .bold()
                             .font(.system(size: 10, weight: .black, design: .default))
@@ -46,12 +34,109 @@ struct HomeLibraryView: View {
                                 bottom: 0,     // 下の余白
                                 trailing: 10    // 右の余白
                             ))
-                    }
+                        Spacer()
+                    }.frame(width: 180,height: 50)
                     .background(Color(red: 35/255, green: 5/255, blue: 5/255))
-                    .cornerRadius(3)
+                        .cornerRadius(3)
+                    HStack {
+                        Image(homeLibraryArray[1].image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50,height: 50)
+                        Text(homeLibraryArray[1].name)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 10, weight: .black, design: .default))
+                            .padding(EdgeInsets(
+                                top: 0,        // 上の余白
+                                leading: 0,    // 左の余白
+                                bottom: 0,     // 下の余白
+                                trailing: 10    // 右の余白
+                            ))
+                        Spacer()
+                    }.frame(width: 180,height: 50)
+                    .background(Color(red: 35/255, green: 5/255, blue: 5/255))
+                        .cornerRadius(3)
+                }
+                HStack() {
+                    HStack {
+                        Image(homeLibraryArray[2].image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50,height: 50)
+                        Text(homeLibraryArray[2].name)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 10, weight: .black, design: .default))
+                            .padding(EdgeInsets(
+                                top: 0,        // 上の余白
+                                leading: 0,    // 左の余白
+                                bottom: 0,     // 下の余白
+                                trailing: 10    // 右の余白
+                            ))
+                        Spacer()
+                    }.frame(width: 180,height: 50).background(Color(red: 35/255, green: 5/255, blue: 5/255))
+                        .cornerRadius(3)
+                    HStack {
+                        Image(homeLibraryArray[3].image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50,height: 50)
+                        Text(homeLibraryArray[3].name)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 10, weight: .black, design: .default))
+                            .padding(EdgeInsets(
+                                top: 0,        // 上の余白
+                                leading: 0,    // 左の余白
+                                bottom: 0,     // 下の余白
+                                trailing: 10    // 右の余白
+                            ))
+                        Spacer()
+                    }.frame(width: 180,height: 50)
+                    .background(Color(red: 35/255, green: 5/255, blue: 5/255))
+                        .cornerRadius(3)
+                }
+                HStack() {
+                    HStack {
+                        Image(homeLibraryArray[4].image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50,height: 50)
+                        Text(homeLibraryArray[4].name)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 10, weight: .black, design: .default))
+                            .padding(EdgeInsets(
+                                top: 0,        // 上の余白
+                                leading: 0,    // 左の余白
+                                bottom: 0,     // 下の余白
+                                trailing: 10    // 右の余白
+                            ))
+                        Spacer()
+                    }.frame(width: 180,height: 50).background(Color(red: 35/255, green: 5/255, blue: 5/255))
+                        .cornerRadius(3)
+                    HStack {
+                        Image(homeLibraryArray[5].image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50,height: 50)
+                        Text(homeLibraryArray[5].name)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 10, weight: .black, design: .default))
+                            .padding(EdgeInsets(
+                                top: 0,        // 上の余白
+                                leading: 0,    // 左の余白
+                                bottom: 0,     // 下の余白
+                                trailing: 10    // 右の余白
+                            ))
+                        Spacer()
+                    }.frame(width: 180,height: 50)
+                    .background(Color(red: 35/255, green: 5/255, blue: 5/255))
+                        .cornerRadius(3)
                 }
             }
-        }
     }
 }
 
--- SwiftUI-SpotifyApp/Views/Home/HomeView.swift
+++ SwiftUI-SpotifyApp/Views/Home/HomeView.swift
@@ -11,24 +11,12 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
-            HStack() {
-                Image("favorite_music")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 40,height: 40)
-                Text("お気に入りの曲")
-                    .foregroundColor(.white)
-                    .bold()
-                    .font(.system(size: 10, weight: .black, design: .default))
-                    .padding(EdgeInsets(
-                      top: 0,        // 上の余白
-                      leading: 0,    // 左の余白
-                      bottom: 0,     // 下の余白
-                      trailing: 10    // 右の余白
-                      ))
+            VStack() {
+               HomeLibraryView()
             }
-            .background(Color(red: 35/255, green: 5/255, blue: 5/255))
+            .background(Color(red: 0/255, green: 0/255, blue: 0/255))
                 .cornerRadius(3)
+                .frame(width: 300,height: 150)
         }
     }
 }
