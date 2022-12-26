c47a2288743778e743b94bcdb143c17bab68b44a
mikekondo <foad5969@gmail.com>
2022-12-26 16:59:31 +0900

    add: toolbarのセット

--- SwiftUI-SpotifyApp/Views/Home/HomeView.swift
+++ SwiftUI-SpotifyApp/Views/Home/HomeView.swift
@@ -9,14 +9,49 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            VStack() {
-               HomeLibraryView()
+        NavigationStack {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                VStack() {
+                   HomeLibraryView()
+                }
+                .background(Color(red: 0/255, green: 0/255, blue: 0/255))
+                    .cornerRadius(3)
+                    .frame(width: 300,height: 150)
+            }.toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        // 何もしない
+                    } label: {
+                        Text("こんにちは")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        // 何もしない
+                    } label: {
+                        Image(systemName: "bell")
+                            .foregroundColor(.white)
+                    }
+
+                    Button {
+                        // 何もしない
+                    } label: {
+                        Image(systemName: "clock")
+                            .foregroundColor(.white)
+                    }
+                    Button {
+                        // 何もしない
+                    } label: {
+                        Image(systemName: "gearshape")
+                            .foregroundColor(.white)
+                    }
+
+
+                }
             }
-            .background(Color(red: 0/255, green: 0/255, blue: 0/255))
-                .cornerRadius(3)
-                .frame(width: 300,height: 150)
         }
     }
 }
