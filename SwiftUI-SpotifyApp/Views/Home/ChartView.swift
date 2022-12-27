b19eecbe5e85b9144efb5dd83a0789fc8885a61f
mikekondo <foad5969@gmail.com>
2022-12-27 16:10:46 +0900

    add: チャートの素材挿入

---
+++ SwiftUI-SpotifyApp/Views/Home/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  SwiftUI-SpotifyApp
+//
+//  Created by 近藤米功 on 2022/12/27.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
