3f17ac4690495d4fe6938fcecd1474873c7bb869
mikekondo <foad5969@gmail.com>
2022-12-27 16:29:16 +0900

    add: チャートview作成

---
+++ SwiftUI-SpotifyApp/Model/Chart.swift
@@ -0,0 +1,14 @@
+//
+//  Chart.swift
+//  SwiftUI-SpotifyApp
+//
+//  Created by 近藤米功 on 2022/12/27.
+//
+
+import Foundation
+
+struct Chart: Identifiable {
+    let id = UUID()
+    let text: String
+    let image: String
+}
--- SwiftUI-SpotifyApp/Views/Home/ChartView.swift
+++ SwiftUI-SpotifyApp/Views/Home/ChartView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ChartView: View {
+    // MARK: - Variables
+    @State private var chartArray = [Chart(text: "Vaundy, BTS, YOASOBI, The 1975, Taylor Swift", image: "hot_hits"),Chart(text: "今、再生回数が一番多い曲（毎日更新）- 日本", image: "japan_top50"), Chart(text: "今、再生回数が一番多い曲（毎日更新）- global", image: "global_top50")]
+
+    // MARK: - View
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("チャート")
+                    .bold()
+                    .font(.title)
+                Spacer()
+            }
+            .padding()
+            ScrollView(.horizontal) {
+                HStack() {
+                    ForEach(chartArray) { chart in
+                        VStack(spacing: 5) {
+                            Image(chart.image)
+                                .resizable()
+                                .frame(width: 150,height: 150)
+                            Text(chart.text)
+                                .frame(width: 150)
+                                .font(.caption)
+                        }
+                    }.padding(.leading,5)
+                }
+            }
+        }
     }
 }
 
